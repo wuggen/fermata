5441261e880479eeb0068e1f62d907f4b73ca2c1
Julia Andrews <theunderdog09@gmail.com>
2026-06-15 14:48:14 -0400

    Add output stream wrapper

--- src/stream.rs
+++ src/stream.rs
@@ -2,16 +2,25 @@
 
 use std::sync::mpsc::{self, Receiver};
 
-use midir::{MidiInput, MidiInputConnection, MidiInputPort};
+use midir::{
+    MidiInput, MidiInputConnection, MidiInputPort, MidiOutput, MidiOutputConnection, MidiOutputPort,
+};
 
 use crate::midi;
 
+/// A MIDI input stream.
+///
+/// This is a thin wrapper around a [`midir::MidiInputConnection`] which
+/// automatically parses incoming messages into [`midi::Message`]s. Its
+/// interface is based on [`Iterator`] rather than using callbacks.
 pub struct MidiInputStream {
     _conn: MidiInputConnection<Option<u8>>,
     recv: Receiver<(u64, midi::Message)>,
 }
 
 impl MidiInputStream {
+    /// Create a new MIDI input stream, opening a connection on the given port
+    /// of the given input device.
     pub fn new(name: &str, input: MidiInput, port: &MidiInputPort) -> Self {
         let (send, recv) = mpsc::sync_channel::<(u64, midi::Message)>(1);
 
@@ -39,7 +48,7 @@ impl MidiInputStream {
                 },
                 None::<u8>,
             )
-            .expect("Failed to open MIDI connection");
+            .expect("Failed to open MIDI input connection");
 
         MidiInputStream { _conn: conn, recv }
     }
@@ -50,5 +59,23 @@ impl Iterator for MidiInputStream {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.recv.recv().ok()
+    }
+}
+
+pub struct MidiOutputStream {
+    conn: MidiOutputConnection,
+}
+
+impl MidiOutputStream {
+    pub fn new(name: &str, output: MidiOutput, port: &MidiOutputPort) -> Self {
+        let conn = output
+            .connect(port, name)
+            .expect("Failed to open MIDI output connection");
+        Self { conn }
+    }
+
+    pub fn send(&mut self, msg: midi::Message) {
+        let bytes = msg.encode(true);
+        self.conn.send(&bytes).expect("Failed to send MIDI message");
     }
 }
